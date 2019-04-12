acf0b70ff7bad5be2e2d05273477f85fdc65a238
annlinros <annlinros@gmail.com>
2019-04-12 17:19:57 +0200

    added more weather icons

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+/* API Key */
 const apiKey = "8f7dfcf3b43d0c401d33a212979b7e78";
 const units = "Metric";
 
-/* When enter key is up, fire getWeather() */
+/* When enter key is up, getWeather() fires */
 function searchWeather(){
     if (event.keyCode === 13) {getWeather()};
 }
@@ -20,41 +20,43 @@ function getWeather(){
             showWeather(data)
         })
 } 
+/* A function to generate different weather icons according to the output from the API */
 function weatherIcon(weather){
-/*     debugger;
- */
-    const main = weather.weather[0].main;
+      
+    const mainweather = weather.weather[0].main;
+    const weather_icon = weather.weather[0].icon;
     let icon = "";
 
-    switch(main) {
-        case "Rain" : icon +=  "weather_icons/rainy-3.svg";
+    switch(mainweather) {
+        case "Rain": if(weather_icon === "09d") { icon += "weather_icons/rainy-7.svg"}
+                    else { icon +=  "weather_icons/rainy-1.svg"};
                     break;
-        case "Thunderstorm" : icon +=  "weather_icons/thunder.svg";
+        case "Thunderstorm": icon +=  "weather_icons/thunder.svg";
                     break;
-        case "Drizzle" : icon +=  "weather_icons/rainy-3.svg";
+        case "Drizzle": icon +=  "weather_icons/rainy-4.svg";
                     break;
-        case "Snow" : icon +=  "weather_icons/snowy-6.svg";
+        case "Snow": icon +=  "weather_icons/snowy-6.svg";
                     break;
-        case "Atmosphere" : icon +=  "http://openweathermap.org/img/w/${weather.weather[0].icon}.png";
+        case "Atmosphere": icon +=  "http://openweathermap.org/img/w/${weather.weather[0].icon}.png";
                     break;
-        case "Clear" : icon +=  "weather_icons/day.svg";
+        case "Clear": if(weather_icon === "01d") {icon +=  "weather_icons/day.svg"}
+                    else { icon +=  "weather_icons/night.svg"};
                     break;
-        case "Clouds" : icon +=  "weather_icons/cloudy.svg";
-                    break;  
+        case "Clouds": if(weather_icon === "02d" || "04d") { icon += "weather_icons/cloudy-day-3.svg"}
+                    else if( weather_icon === "02n" || "04n"){ icon +=  "weather_icons/cloudy-night-3.svg"}
+                    else { icon +=  "weather_icons/cloudy.svg" };
+                    break; 
+        default: icon +=  "weather_icons/day.svg";
+                    break;
     }
     return icon;
-    console.log(main);
-    console.log(icon);
+}  
 
 
-}         
 /* Displaying the weather information */
 function showWeather(weather){
 
-/*     const weatherIcon = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
- */  
     document.querySelector(".output").innerHTML = 
-    
         `<div class="weather_results">
             <h2 class="city">${weather.name}, ${weather.sys.country}</h2>
             <div class="temp">${Math.round(weather.main.temp)}°C</div>
@@ -68,8 +70,7 @@ function showWeather(weather){
             <p>Winds at ${weather.wind.speed} m/s</p>
             <p>Humidity levels at ${weather.main.humidity}%</p>
         </div>`
-
-
 }
+
 /* Event Listener for Enter key up */      
 document.querySelector(".user_search").addEventListener("keyup", searchWeather);
